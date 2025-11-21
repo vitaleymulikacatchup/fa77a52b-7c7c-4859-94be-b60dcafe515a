3c38a0eabeb28d2650f606700a9d2d346f3848d5
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-11-21 17:00:11 +0200

    Update project files

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,35 +1,32 @@
-"use client";
-
-import { memo } from "react";
-import ButtonHoverMagnetic from "./ButtonHoverMagnetic/ButtonHoverMagnetic";
-import ButtonIconArrow from "./ButtonIconArrow";
-import ButtonShiftHover from "./ButtonShiftHover/ButtonShiftHover";
 import ButtonTextStagger from "./ButtonTextStagger/ButtonTextStagger";
+import ButtonShiftHover from "./ButtonShiftHover/ButtonShiftHover";
+import ButtonHoverMagnetic from "./ButtonHoverMagnetic/ButtonHoverMagnetic";
 import ButtonTextUnderline from "./ButtonTextUnderline";
+import ButtonIconArrow from "./ButtonIconArrow";
 import ButtonHoverBubble from "./ButtonHoverBubble";
 import ButtonExpandHover from "./ButtonExpandHover";
-import { useRouter } = "next/navigation";
+import { useRouter } from "next/navigation";
 import type { ButtonVariantProps } from "./types";
 
 export type { ButtonVariant, ButtonVariantProps, ButtonPropsForVariant } from "./types";
 
-const buttonComponents = {
-  "hover-magnetic": ButtonHoverMagnetic,
-  "hover-bubble": ButtonHoverBubble,
-  "expand-hover": ButtonExpandHover,
-  "icon-arrow": ButtonIconArrow,
-  "shift-hover": ButtonShiftHover,
-  "text-stagger": ButtonTextStagger,
-  "text-underline": ButtonTextUnderline,
-} as const;
-
-const Button = (props: ButtonVariantProps) => {
-  const router = useRouter();
-  const { variant = "hover-magnetic", href, ...restProps } = props;
-  const ButtonComponent = buttonComponents[variant];
-  return <ButtonComponent {...restProps} onClick={() => router.push(href)} />;
-};
-
-Button.displayName = "Button";
-
-export default memo(Button);
+export default function Button({ variant, ...props }: ButtonVariantProps) {
+  switch (variant) {
+    case "text-stagger":
+      return <ButtonTextStagger {...props} />;
+    case "shift-hover":
+      return <ButtonShiftHover {...props} />;
+    case "hover-magnetic":
+      return <ButtonHoverMagnetic {...props} />;
+    case "text-underline":
+      return <ButtonTextUnderline {...props} />;
+    case "icon-arrow":
+      return <ButtonIconArrow {...props} />;
+    case "hover-bubble":
+      return <ButtonHoverBubble {...props} />;
+    case "expand-hover":
+      return <ButtonExpandHover {...props} />;
+    default:
+      return <ButtonTextStagger {...props} />;
+  }
+}
